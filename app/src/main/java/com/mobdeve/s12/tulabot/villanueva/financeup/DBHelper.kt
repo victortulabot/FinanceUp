33a8375fd5892e20077d19856afbf8da4aef31b2
Victor Tulabot <44962471+victortulabot@users.noreply.github.com>
2022-01-03 16:15:51 +0800

    Main Activity Changes
    - Tried db
    - hiding confirm password

---
+++ app/src/main/java/com/mobdeve/s12/tulabot/villanueva/financeup/DBHelper.kt
@@ -0,0 +1,76 @@
+package com.mobdeve.s12.tulabot.villanueva.financeup
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper(context: Context?) :
+    SQLiteOpenHelper(context, DATABASENAME, null, DATABASEVERSION){
+
+    override fun onCreate(db: SQLiteDatabase){
+        db.execSQL(CREATE_USER_TABLE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLEUSERS)
+        onCreate(db)
+    }
+
+    companion object {
+        private const val DATABASENAME = "FinanceUp.db"
+        private const val DATABASEVERSION = 1
+
+        //column names
+        const val TABLEUSERS = "users"
+        const val USERSID = "id"
+        const val USERSNAME = "username"
+        const val USERSPASS = "userpass"
+        private const val CREATE_USER_TABLE = ("create table " + TABLEUSERS + "( "
+                + USERSID + " integer primary key autoincrement, "
+                + USERSNAME + " text, "
+                + USERSPASS + " text ); ")
+    }
+
+    fun insertDataUser(username: String, password: String){
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put(USERSNAME, username)
+        cv.put(USERSPASS, password)
+        db.insert(TABLEUSERS, null, cv)
+
+        db.close()
+    }
+
+    fun checkDataUser(username: String):Boolean{
+        var boolResult = false
+        val db = this.writableDatabase
+        val query = "Select * from users where username = " + username + ""
+        val result = db.rawQuery(query, null )
+
+        if (result.count > 0){
+            boolResult = true
+        }
+
+        result.close()
+        db.close()
+
+        return boolResult
+    }
+
+    fun loginDataUser(username: String, password: String):Boolean{
+        var boolResult = false
+        val db = this.writableDatabase
+        val query = "Select * from users where username = " + username + " and userpass = " + password + ""
+        val result = db.rawQuery(query, null )
+
+        if (result.count > 0){
+            boolResult = true
+        }
+
+        result.close()
+        db.close()
+
+        return boolResult
+    }
+}
--- app/src/main/java/com/mobdeve/s12/tulabot/villanueva/financeup/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s12/tulabot/villanueva/financeup/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.mobdeve.s12.tulabot.villanueva.financeup
 
 import android.content.Intent
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import com.mobdeve.s12.tulabot.villanueva.financeup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +18,82 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
-        binding!!.btnLogin.setOnClickListener{
-            val gotoDashboardActivity = Intent(applicationContext, DashboardActivity:: class.java)
+        var db = DBHelper(this)
 
-            startActivity(gotoDashboardActivity)
-            finish()
+        binding!!.btnLog.setOnClickListener {
+            binding!!.etConfirmPassword.visibility = View.INVISIBLE;
+            binding!!.btnRegister.backgroundTintList = getColorStateList(android.R.color.white);
+            binding!!.btnLog.backgroundTintList = getColorStateList(android.R.color.darker_gray);
+            binding!!.btnLogin.text = "Log In";
+        }
+
+        binding!!.btnRegister.setOnClickListener {
+            binding!!.etConfirmPassword.visibility = View.VISIBLE;
+            binding!!.btnLog.backgroundTintList = getColorStateList(android.R.color.white);
+            binding!!.btnRegister.backgroundTintList = getColorStateList(android.R.color.darker_gray);
+            binding!!.btnLogin.text = "Register";
+        }
+
+        binding!!.btnLogin.setOnClickListener{
+            val username = binding!!.etUsername.text.toString();
+            val password = binding!!.etPassword.text.toString();
+            val cPassword = binding!!.etConfirmPassword.text.toString();
+
+            if (binding!!.btnLogin.text.toString() == "Register"){
+                if (username == "" || password == "" || cPassword == ""){
+                    Toast.makeText(
+                        applicationContext,
+                        "Please input all required fields!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else if (password != cPassword){
+                    Toast.makeText(
+                        applicationContext,
+                        "Password does not match!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    if(db.checkDataUser(username)){
+                        Toast.makeText(
+                            applicationContext,
+                            "Username already exists!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        db.insertDataUser(username, password);
+
+                        val gotoDashboardActivity = Intent(applicationContext, DashboardActivity:: class.java)
+
+                        startActivity(gotoDashboardActivity)
+                        finish()
+                    }
+                }
+            }
+            else {
+                if (username == "" || password == ""){
+                    Toast.makeText(
+                        applicationContext,
+                        "Please input all required fields!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+//                    var log = db.loginDataUser(username, password)
+                    var log = true
+                    if (log){
+                        val gotoDashboardActivity = Intent(applicationContext, DashboardActivity:: class.java)
+
+                        startActivity(gotoDashboardActivity)
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            applicationContext,
+                            "Invalid credentials!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+
+                }
+            }
         }
     }
 }
